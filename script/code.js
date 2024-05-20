730b4c478770aef8a508ceebc3a1a3579c35c922
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-05-20 11:05:00 +0200

    complete

--- script/code.js
+++ script/code.js
@@ -1,4 +1,38 @@
-let num1 = document.querySelector(['num1']).value
-let num2
+/* function CtF() {
+    let display = querySelector('[display]')
+    let num1 = document.querySelector(['num1']).value
+    let output = +num1 * 9 / 5 + 32
+    display.value = (output)
+    // console.log(output)
+}
 
-console.log(num1)
+let button1 = document.querySelector('[CtF]')
+button1.addEventListener('click', CtF) */
+
+function CtF(){
+
+    let numb1 = document.querySelector('[numb1]').value
+
+    if (numb1 == '') {
+        alert('Please input a number')
+        document.querySelector('[display]').value = ''
+    } else {
+        let output = +numb1 * 9 / 5 + 32
+        document.querySelector('[display]').value = output.toFixed(2)
+    }
+   
+}
+function FtC(){
+  
+    let numb1 = document.querySelector('[numb1]').value
+
+    if (numb1 == '') {
+        alert('Please input a number')
+        document.querySelector('[display]').value = ''
+    } else {
+        let output = (+numb1 -32) * 5 / 9
+        document.querySelector('[display]').value = output.toFixed(2)
+    }
+ 
+}
+
